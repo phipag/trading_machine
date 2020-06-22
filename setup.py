49c69a11a236ced30a2b530be02af08af8de2c68
Philipp Page <github@philipp-page.de>
2020-06-22 12:28:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='tm',
     version='0.0.1dev1',
     description='An automated trading machine which finds itself a good trading strategy',
-    author='Philipp Page',
+    author='Philipp Page, Jonathan Feldges, Nico Hakenes',
     author_email='ppagejem@smail.uni-koeln.de',
     packages=find_packages('*'),
-    install_requires=['pandas', 'yfinance'],
+    install_requires=['pandas', 'yfinance', 'deap', 'numpy'],
 )
