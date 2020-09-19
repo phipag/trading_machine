a3955acaf9a72ca2d3d6a02451a82b5faa0cd0fc
phipag <github@philipp-page.de>
2020-09-19 11:50:30 +0100

    Formatting.

--- tm/trading_rules/__init__.py
+++ tm/trading_rules/__init__.py
@@ -1,9 +1,8 @@
+from tm.trading_rules.ChandelierExit import ChandelierExit
 from tm.trading_rules.ExponentialMovingAverage import ExponentialMovingAverage
 from tm.trading_rules.MACD import MACD
-from tm.trading_rules.RSI import RSI
 from tm.trading_rules.ROC import ROC
+from tm.trading_rules.RSI import RSI
 from tm.trading_rules.STO import STO
-from tm.trading_rules.ChandelierExit import ChandelierExit
 from tm.trading_rules.SimpleMovingAverage import SimpleMovingAverage
 from tm.trading_rules.TradingRule import TradingRule
-
