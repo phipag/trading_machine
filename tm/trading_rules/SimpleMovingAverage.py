5b4336f1576c6844d926f6efa0203bd6c5c05cab
Jonathan Feldges <jojofeldges@web.de>
2020-09-20 21:17:43 +0200

    changed num_bits again

--- tm/trading_rules/SimpleMovingAverage.py
+++ tm/trading_rules/SimpleMovingAverage.py
@@ -1,14 +1,12 @@
 from typing import List
-
 import pandas as pd
-
 from tm import StockDataProvider
 from tm.trading_rules.TradingRule import TradingRule
 
 
 class SimpleMovingAverage(TradingRule):
-    # The days parameters needs 6 bits (= all integers in [0, 63)
-    num_bits: List[int] = [6]
+    # The days parameters needs 8 bits (= all integers in [0, 127])
+    num_bits: List[int] = [8]
 
     def __init__(self, stock_data_provider: StockDataProvider, days: int = 200):
         super().__init__(stock_data_provider)
